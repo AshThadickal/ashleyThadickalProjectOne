ecd35ed25aa408b431cbca1490528589c500dd6e
Ashley <ashleythadickal@gmail.com>
2021-12-15 09:58:18 -0500

    BIG change to menu and addition of comment adds

--- scripts.js
+++ scripts.js
@@ -1,57 +1,64 @@
-// Create functionality with the hamburger menu
-// 1. create an event listener on the <i> that is the menu image
-// 2. when clicked, populate a ul with the <a> to direct to the different pages (i.e. home, blog and contact us) - make this a toggle such that it can be closed again? or create an 'X' on top corner to close?
+// // Create functionality with the hamburger menu
+// // 1. create an event listener on the <i> that is the menu image
+// // 2. when clicked, show the menu 
 
-// creating a variable targetting the <i> that is the menu icon
-const menuIcon = document.querySelector('.fa-bars')
-// console.log(menuIcon);
-// creating the listener event
+const menuIcon = document.querySelector('.fa-bars');
+const menuList = document.querySelector('.hamburgerMenuDisplay')
+const menuUl = document.querySelector('.hamburgerMenuDisplay ul')
+
+// add my event listener to the menuIcon
+
 menuIcon.addEventListener('click', function() {
-    console.log(menuIcon)
-    
-    // I want to add a div that will house a ul enclosing the menu items
-    const newDiv = document.createElement('div');
-    // console.log(newUl);
-
-    // append the div after the end of the menuIcon
-     menuIcon.insertAdjacentElement('afterend', newDiv);
-
-    // Need to create the ul as innerHTML to the div to house the li items
-    const newUl = document.createElement ('ul');
-    // append this ul to the div container
-    newDiv.insertAdjacentElement('afterbegin', newUl);
-    // change style to display block
-    newUl.style.display = 'block';
-
-
-    // create a function that will append each li element to the ul 
-    function appendlI(listItem) {
-        // each time I call this function, I want to create a new li item
-        const newLi = document.createElement ('li');
-        // need to get the index items within the li element
-        newLi.innerHTML = listItem
-        // I need to change the display back to block because in media queries it is at display none for the <ul>
-        // listItem.style.dispay = 'block';
-        // I then want to append each li to the end of the ul element
-        newUl.appendChild(newLi)
-
-        // const menuItems = document.createElement('li');
-        // newUl.appendChild(menuItems);   
-    }
-    
-    appendlI('<a href="#home">home</a>');
-    appendlI('<a href="blog.html">blog</a>');
-    appendlI('<a href="#portfolio">portfolio</a>');
-    appendlI('<a href="#shop">shop</a>');
-    appendlI('<a href="contact.html">contact</a>');
- 
-   
-    
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-times-circle');
+    menuUl.style.display = 'block'
+    menuList.classList.toggle('javaMenu');
 })
 
+// adding comments to the blog page within the commentSection area
+// 1. on submit, take the name and comment user input and save a variables
+// 2. append to the 
+
+// targetting the form 
+const formElement = document.querySelector('.leaveComment form')
+
+// adding the event listener on submit
+formElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // store the name value
+    const nameElement = document.querySelector('input[name="clientName"]');
+    const userName = nameElement.value;
+
+    // store the comment value
+    const commentElemnt = document.querySelector('#clientComment')
+    const userComment = commentElemnt.value;
+
+    console.log(userName);
+    console.log(userComment)
+    // create a new div to house the nameElement and commentElement
+    const divElement = document.createElement('div');
+    divElement.className = 'newComment';
+    console.log(divElement);
+
+    // append the new div to the commentSection
+    document.querySelector('.addedComments').append(divElement);
+
+    // create a <p> element for both the name and comment to live in
+
+    const nameParagraph = document.createElement('p');
+    nameParagraph.textContent = userName;
+
+    const commentParagraph = document.createElement('p');
+    commentParagraph.textContent = userComment;
+
+    // add the namePara. and commentPara. values to the new div
+    divElement.appendChild(nameParagraph);
+    divElement.appendChild(commentParagraph);
 
 
-// newUl.innerHTML = '<li>< a href = "blog.html" > blog</a ></li >';
-//     newUl.innerHTML = '<li>< a href = "#portfolio" > portfolio</a ></li >'
-//     newUl.innerHTML = '<li>< a href = "#shop" > shop</a ></li>'
-//     newUl.innerHTML = '<li><a href="contact.html">contact</a></li>'
+    // need to create <p> elements for both and add textontent to both then append each to the new divElement then add that divElement to the end of the commentSection... might need to change some IDs, etc. so that it is just on the blog page
+    
+
+
+})
