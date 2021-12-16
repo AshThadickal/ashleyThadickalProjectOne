b75b2fc795d528c720bc7299dbfc3cee16e8c426
Ashley <ashleythadickal@gmail.com>
2021-12-15 22:19:04 -0500

    Final with Namespacing

--- scripts.js
+++ scripts.js
@@ -1,30 +1,46 @@
-// // Create functionality with the hamburger menu
-// // 1. create an event listener on the <i> that is the menu image
-// // 2. when clicked, show the menu 
+// A. Create functionality with the hamburger menu:
+//  1. create an event listener on the <i> that is the menu image
+//  2. when clicked, show the menu 
 
-const menuIcon = document.querySelector('.fa-bars');
-const menuList = document.querySelector('.hamburgerMenuDisplay')
-const menuUl = document.querySelector('.hamburgerMenuDisplay ul')
+// B. Adding comments to the blog page within the commentSection area:
+//  1. on submit, take the name and comment user input and save as variables
+//  2. append the variables to the new comment section and clear the form
 
-// add my event listener to the menuIcon
+const creativeApp = {};
 
-menuIcon.addEventListener('click', function() {
-    menuIcon.classList.toggle('fa-bars');
-    menuIcon.classList.toggle('fa-times-circle');
-    menuUl.style.display = 'block'
-    menuList.classList.toggle('javaMenu');
-})
+creativeApp.init = function(){
+    creativeApp.attachMenuListener();        
+    creativeApp.attachFormListener();
+}    
 
-// adding comments to the blog page within the commentSection area
-// 1. on submit, take the name and comment user input and save a variables
-// 2. append to the 
 
-// targetting the form 
-const formElement = document.querySelector('.leaveComment form')
+// create a function to hold the menu listener event to make menu functional
+creativeApp.attachMenuListener = function() {
+    // using const within the function as it would be protected within the function 
+    const menuIcon = document.querySelector('.fa-bars');
+    const menuList = document.querySelector('.hamburgerMenuDisplay')
+    const menuUl = document.querySelector('.hamburgerMenuDisplay ul')
+    
+    // adding event listener to the menuIcon such that a menu will pop down when clicked
+    menuIcon.addEventListener('click', function() {
+        menuIcon.classList.toggle('fa-bars');
+        menuIcon.classList.toggle('fa-times-circle');
+        menuUl.style.display = 'block'
+        menuList.classList.toggle('javaMenu');
+    })
+     
+}
 
-// adding the event listener on submit
-formElement.addEventListener('submit', function(event) {
-    event.preventDefault();
+
+
+// Create a function to hold the event listener to add comments to the page
+creativeApp.attachFormListener = function() {
+    // targetting the form 
+    const formElement = document.querySelector('.leaveComment form')
+
+    // adding the event listener on submit
+    formElement.addEventListener('submit', function(event) {
+        event.preventDefault();
     
     // store the name value
     const nameElement = document.querySelector('input[name="clientName"]');
@@ -34,31 +50,39 @@ formElement.addEventListener('submit', function(event) {
     const commentElemnt = document.querySelector('#clientComment')
     const userComment = commentElemnt.value;
 
+    // error handling to prevent empty name/comment
     if (userName && userComment) {
-   // create a new div to house the nameElement and commentElement
-    const divElement = document.createElement('div');
-    divElement.className = 'newComment';
+        // create a new div to house the nameElement and commentElement
+        const divElement = document.createElement('div');
+        divElement.className = 'newComment';
     
+        // append the new div to the commentSection
+        document.querySelector('.addedComments').append(divElement);
 
-    // append the new div to the commentSection
-    document.querySelector('.addedComments').append(divElement);
+        // create a <p> element for both the name and comment to live in
+        const nameParagraph = document.createElement('p');
+        nameParagraph.textContent = userName;
 
-    // create a <p> element for both the name and comment to live in
+        const commentParagraph = document.createElement('p');
+        commentParagraph.textContent = userComment;
 
-    const nameParagraph = document.createElement('p');
-    nameParagraph.textContent = userName;
-
-    const commentParagraph = document.createElement('p');
-    commentParagraph.textContent = userComment;
-
-    // add the namePara. and commentPara. values to the new div
-    divElement.append(nameParagraph, commentParagraph);
+        // add the namePara. and commentPara. values to the new div
+        divElement.append(nameParagraph, commentParagraph);
     
+        // need to clear the input field
+        nameElement.value = '';
+        commentElemnt.value = '';
+        } else if (userName === '' && userComment === '') {
+            alert('Please provide us with your name and comment.');
+        } else if (userComment === '') {
+            alert('Please provide a comment.');
+        } else if (userName === '') {
+            alert('Please provide your name.')
+        }
+    })
+}
 
-    // need to clear the input field
-    nameElement.value = '';
-    commentElemnt.value = '';
-    }
+
+creativeApp.init();
 
 
-})
